472f42232fd92bc10e1f28621ea0f5d3645a6154
Tyler Ewing <tewing10@gmail.com>
2013-11-19 17:09:03 -0500

    Adding in Segmentation tests

--- src/ocr/segmentation/Segmentation.java
+++ src/ocr/segmentation/Segmentation.java
@@ -13,7 +13,7 @@ public class Segmentation {
         return Segmentation.segmentWords(G);
     }
     
-    public static List<BufferedImage> segmentWords(BufferedImage G){
+    public static List<BufferedImage> segmentWords(BufferedImage G) {
         
         List<BufferedImage> letters = new LinkedList<>();
         List<Integer> currentWord = null;
@@ -36,16 +36,67 @@ public class Segmentation {
             }            
         }
         
+        // TODO: False, this needs fixed
         // There should not be any trailing whitespace
         int width = currentWord.get(currentWord.size()-1) - currentWord.get(0);
         int height = G.getHeight();
         BufferedImage subimage = G.getSubimage(currentWord.get(0), 0, width, height);
         letters.add(subimage);
         
-        // TODO: Take out when functioning properly
-//        letters.add(G);
-        
         return letters;
+    }
+    
+    private static List<BufferedImage> segmentCharacters(BufferedImage G) {
+        List<BufferedImage> characters = new LinkedList<>();
+        List<Integer> currentChar = null;
+        int height = G.getHeight();
+        
+        return characters;
+    }
+    
+    private static List<BufferedImage> segmentLines(BufferedImage G) {        
+        List<BufferedImage> lines = new LinkedList<>();
+        List<Integer> currentLine = null;
+        int width = G.getWidth();
+        
+        for(int y = 0; y < G.getHeight(); ++y) {
+            
+            if(rowHasBlackPixel(G, y)) {
+                if(currentLine == null) { currentLine = new ArrayList<>(); }
+                currentLine.add(y);
+            }
+            else if(currentLine != null) {
+                int height = currentLine.get(currentLine.size()-1) - currentLine.get(0);
+                BufferedImage subImage = G.getSubimage(0, currentLine.get(0), width, height);
+                lines.add(subImage);
+                ImageHelper.printImage(subImage, "segmented_line.png");
+                currentLine = null;
+            }
+        }
+        
+        if(currentLine != null) {
+            int height = currentLine.get(currentLine.size()-1) - currentLine.get(0);
+            BufferedImage subImage = G.getSubimage(0, currentLine.get(0), width, height);
+            lines.add(subImage);            
+        }
+        
+        return lines;
+    }
+    
+    private static boolean rowHasBlackPixel(BufferedImage G, int row) {        
+        for(int x = 0; x < G.getWidth(); ++x) {
+            int color = Math.abs(G.getRGB(x, row));
+            if(color > Config.THRESHOLD) { return true; }
+        }
+        return false;
+    }
+    
+    private static boolean colHasBlackPixel(BufferedImage G, int col) {
+        for(int y = 0; y < G.getHeight(); ++y) {
+            int color = Math.abs(G.getRGB(col, y));
+            if(color > Config.THRESHOLD) { return true; }
+        }
+        return false;
     }
     
     private static boolean hasBlackPixel(BufferedImage G, int col) {
@@ -56,4 +107,9 @@ public class Segmentation {
         }
         return false;
     }
+    
+    
+    
+    // Experimental
+//    private 
 }
---
+++ src/ocr/testing/SegmentationTesting.java
@@ -0,0 +1,20 @@
+package ocr.testing;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.imageio.ImageIO;
+import ocr.segmentation.Segmentation;
+
+public class SegmentationTesting {
+    
+    public static void main(String[] args) throws IOException {
+        
+        Segmentation seg = new Segmentation();
+        BufferedImage bimg = ImageIO.read(new File(""));
+        List<BufferedImage> images = seg.segment(null);
+        
+         
+    }
+}
